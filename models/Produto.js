9ef8648d2435cd5fbe8038e3da976e45efb8277f
Yproshot <maiconpacheco@live.com>
2024-03-20 21:53:39 -0300

    env-example

---
+++ models/Produto.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const produtoSchema = mongoose.Schema({
+
+    nome: String,
+    preco: Number,
+    tamanho: String,
+    tipo: String
+})
+
+const Produto = mongoose.model('Produtos', produtosSchema)
+
+module.exports = Produto
